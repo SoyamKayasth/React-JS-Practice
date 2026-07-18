0b7ea8c1dd008d746f3181de72e2cb06127e0bb3
SoyamKayasth <megharjmk88@gmail.com>
2026-07-18 15:29:47 +0530

    added a delete button in the Todo App

--- 5. To-do App using React/src/Todo.jsx
+++ 5. To-do App using React/src/Todo.jsx
@@ -1,27 +1,54 @@
 import './todo.css';
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 export default function Todo(){
   
- const [tasks, setTasks] = useState(["Sample Task"]);
-
+ const [tasks, setTasks] = useState([{id : uuid(), task : "Sample Task"}]);
+  
  const [newTodo, setNewTodo] = useState("");
  
-   let getNewTodo = (e) => {+
+ let inputBox = document.querySelector(".inputBox");
+
+   const getNewTodo = (e) => {+
       setNewTodo(e.target.value);
       console.log(newTodo); 
    };
+
+   const addTask = () => {
+    if(inputBox.value !== null && inputBox.value.trim() !== ""){
+      setTasks((prevTasks) => (
+      [...prevTasks, {task : newTodo , id : uuid()}]
+    ));
+     setNewTodo("");   
+     inputBox.value = "";
+    }
+   }
+
+  const deleteTask = (taskId) => {
+       setTasks((prevTasks) => prevTasks.filter((task) => task.id != taskId));
+  }
+
     return(
      <>
-        <div className="container" >
+        <div className="container fixed" >
          <input type="text" name="task" id="task" className="inputBox" placeholder='Enter a task' onChange={getNewTodo}/>
 
-         <button type="submit" className="btn">Add Task</button>
+         <button type="submit" className="btn" onClick={addTask}>Add Task</button>
         </div>
-      <div className="container">
+      <div className="list-container">
          <ul>
-           {tasks.map((task) => (<li>{task}</li>))}
-
+           {tasks.map((task) => (
+            <li key = {task.id} className='listItem'>
+              <div>{task.task} </div>
+              <div>
+                  <button className="editBtn">
+                     edit
+                  </button>
+                <button onClick={() => deleteTask(task.id)} className='fa-solid fa-circle-xmark delBtn'></button> 
+              </div>
+            </li> 
+            ))}
          </ul>
          
       </div>
