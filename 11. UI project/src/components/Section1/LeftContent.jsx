ce8341aef25dea82110bd3bb7eb1e1d3349bbc86
Soyam Kayasth <megharjmk88@gmail.com>
2026-08-11 22:11:26 +0530

    Installed remixicon to add icons

--- 11. UI project/src/components/Section1/LeftContent.jsx
+++ 11. UI project/src/components/Section1/LeftContent.jsx
@@ -4,10 +4,11 @@ function LeftContent() {
   return (
     <div className='bg-pink-600 h-full w-1/4'>LeftContent
        <div>
-
+          <h1>This is heading</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem officiis laborum veritatis. Tempora dolores, ipsum quaerat repellat placeat harum explicabo, possimus unde illo, amet numquam voluptates facilis dolor facere ex.</p>
        </div>
        <div>
-            
+           <i class="ri-arrow-right-up-line"></i>  
        </div>
 
     </div>
